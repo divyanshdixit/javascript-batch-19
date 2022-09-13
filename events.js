1d83356a62808d59787be5cfdd508b8d9abc498e
Divyansh Dixir <divyanshdixit96@gmail.com>
2022-09-14 00:27:12 +0530

    events continue

---
+++ events.js
@@ -0,0 +1,16 @@
+// eventName => eventhanlder(function) 
+// inline js events
+// using html dom => 
+// addEventListener() => event bubbling and event capturing 
+
+/*
+DOCUMEMT OBJECT MODEL
+    window (object)
+    document
+    html
+head            body
+meta title  h1 button div p (object)
+
+// document.getElementById('heading')
+
+*/
